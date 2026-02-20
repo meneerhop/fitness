a77641b7061437be8bc067f98c9afaf14bb6ce98
meneerhop <mitchellhop@hotmail.com>
2026-02-20 13:43:14 +0100

    Update

--- assets/nav.js
+++ assets/nav.js
@@ -1,4 +1,19 @@
-
 function toggleMenu(){
-  document.getElementById("dropdownMenu").classList.toggle("show");
+  const menu = document.getElementById("dropdownMenu");
+  menu.classList.toggle("show");
+}
+
+// Sluit menu als je buiten klikt
+document.addEventListener("click", function(e){
+  const menu = document.getElementById("dropdownMenu");
+  const btn = document.querySelector(".menu-btn");
+
+  if(menu && !menu.contains(e.target) && !btn.contains(e.target)){
+    menu.classList.remove("show");
+  }
+});
+
+function toggleProfileSelect(){
+  const menu = document.getElementById("profileSelectMenu");
+  menu.classList.toggle("show");
 }
