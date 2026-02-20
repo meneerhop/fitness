94f3616b766f2f0fdb6ba45f60cd60e8a70cb101
meneerhop <mitchellhop@hotmail.com>
2026-02-20 15:24:59 +0100

    1.7

--- assets/nav.js
+++ assets/nav.js
@@ -1,15 +1,7 @@
 
 function toggleProfile(){
   const menu = document.getElementById("profileMenu");
-
-  if(menu.classList.contains("show")){
-    menu.classList.remove("show");
-  } else {
-    menu.style.display = "flex";
-    requestAnimationFrame(() => {
-      menu.classList.add("show");
-    });
-  }
+  menu.classList.toggle("show");
 }
 
 function toggleNav(){
